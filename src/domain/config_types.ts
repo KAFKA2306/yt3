832caf99d41ba8a0e01535370945b130cc1d8f18
root <root@frontier.localdomain>
2026-05-17 18:45:37 +0900

    refactor(type): add cognitive design tokens to config interfaces

--- src/domain/config_types.ts
+++ src/domain/config_types.ts
@@ -52,6 +52,16 @@ export interface DesignTokens {
 	font_display: string;
 	font_text: string;
 	font_japanese: string;
+
+	// Humanity Observatory (Cognitive) Palette
+	cognitive_base_white: string;
+	cognitive_soft_sky: string;
+	cognitive_human_cream: string;
+	cognitive_evening_orange: string;
+	cognitive_gentle_blue: string;
+	cognitive_soft_gray: string;
+	cognitive_text_charcoal: string;
+	cognitive_accent_pink: string;
 }
 export interface AppConfig {
 	global_style: GlobalStyle;
