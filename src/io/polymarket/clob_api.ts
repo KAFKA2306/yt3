d124de21b5a96b9131b30226251d453ef2e43f51
root <root@frontier.localdomain>
2026-05-17 15:42:02 +0900

    refactor: purge unused Polymarket domain and IO modules

--- src/io/polymarket/clob_api.ts
+++
@@ -1,6 +0,0 @@
-export class ClobAPI {
-	async placeOrder(order: unknown): Promise<unknown> {
-		console.log("Placing order via CLOB API:", order);
-		return { success: true, id: "clob_123" };
-	}
-}
